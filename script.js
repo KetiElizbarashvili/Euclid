070a4240c005a99105fb63c6d2ef66b505e03dae
KetiElizbarashvili <elizbarashviliketi44@gmail.com>
2023-02-11 12:00:00 +0000

    24

--- script.js
+++ script.js
@@ -4,10 +4,7 @@
 //NAVIGATION
 function myFunction() {
     var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-    x.className += " responsive";
-    } else {
-    x.cl
+
     
     
     
