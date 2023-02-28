c54690d01af9333f80d0fd4ec20a06661eb5189f
KetiElizbarashvili <elizbarashviliketi44@gmail.com>
2023-02-28 12:00:00 +0000

    123

--- script.js
+++ script.js
@@ -19,14 +19,7 @@ function myFunction() {
     window.onscroll = function() {scrollFunction()};
     
     function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("myTopnav").style.backgroundColor = "#1e1e1e";
-    document.getElementById("myTopnav").style.boxShadow = "0px 6px 16px -6px rgba(1,1,1,0.5)";
 
-    
-    
-    
-    
     
     //CLIENT LOGO SLIDER
     $(function() {
